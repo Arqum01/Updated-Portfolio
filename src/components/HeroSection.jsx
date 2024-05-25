ca06dadfc2214481dbe717d92ce05b459b93e287
Arqum01 <arqumahmad019@gmail.com>
2024-05-25 13:25:57 +0500

    Added the Hero Section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <>
+      <div className="bg-gray-900">
+        <section className="mt-48 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
+          <div className="space-y-4 flex-1 sm:text-center lg:text-left">
+            <h1 className="text-white font-bold text-4xl xl:text-5xl">
+              Arqum Ahmad
+              <span className="text-indigo-400"> Frontend React Developer</span>
+            </h1>
+            <p className="text-gray-300 max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
+            I'm Arqum Ahmad, a Frontend React Developer skilled in creating dynamic and responsive web applications. I specialize in using React to build seamless user interfaces with clean and efficient code.
+            </p>
+            <div className="pt-2 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
+              <button class="contactButton">
+                Contact
+                <div class="iconButton">
+                  <svg
+                    height="24"
+                    width="24"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                    <path
+                      d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </div>
+              </button>
+            </div>
+          </div>
+          <div className="flex-1 text-center mt-7 lg:mt-0 lg:ml-3">
+            <img
+              src="https://i.postimg.cc/HxHyt53c/undraw-heatmap-uyye.png"
+              className="w-full mx-auto sm:w-10/12  lg:w-full"
+            />
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default HeroSection;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,77 +1,97 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export function Navbar() {
-
-  const [state, setState] = useState(false)
+  const [state, setState] = useState(false);
 
   const navigation = [
-      { title: "About", path: "javascript:void(0)" },
-      { title: "Experience", path: "javascript:void(0)" },
-      { title: "Skills", path: "javascript:void(0)" },
-      { title: "Portfolio", path: "javascript:void(0)" },
-  ]
+    { title: "About", path: "javascript:void(0)" },
+    { title: "Experience", path: "javascript:void(0)" },
+    { title: "Skills", path: "javascript:void(0)" },
+    { title: "Portfolio", path: "javascript:void(0)" },
+  ];
 
   return (
-      <nav className="bg-transparent w-full border-b md:border-0 md:static">
-          <div className="items-center px-4 mx-auto md:flex md:px-8">
-              <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                    <a href="javascript:void(0)">
-                        <p className='font-bold italic text-2xl text-white'>Arqum Ahmad</p>
-                    </a>
-                  <div className="md:hidden">
-                      <button className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
-                          onClick={() => setState(!state)}
-                      >
-                          {
-                              state ? (
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                                      <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                                  </svg>
-                              ) : (
-                                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8h16M4 16h16" />
-                                  </svg>
-                              )
-                          }
-                      </button>
-                  </div>
-              </div>
-              <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${ state ? 'block' : 'hidden'}`}>
-                  <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                      {
-                          navigation.map((item, idx) => {
-                              return (
-                                <li key={idx} className="text-white hover:text-indigo-600 text-2xl">
-                                    <a href={item.path}>
-                                        { item.title }
-                                    </a>
-                                </li>
-                              )
-                          })
-                      }
-                  </ul>
-              </div>
-              <div className="hidden md:inline-block">
-              <button class="contactButton">
-  Contact
-  <div class="iconButton">
-    <svg
-      height="24"
-      width="24"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M0 0h24v24H0z" fill="none"></path>
-      <path
-        d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-        fill="currentColor"
-      ></path>
-    </svg>
-  </div>
-</button>
-
-              </div>
+    <nav className="bg-transparent w-full border-b md:border-0 md:static">
+      <div className="items-center px-4 mx-auto md:flex md:px-8">
+        <div className="flex items-center justify-between py-3 md:py-5 md:block">
+          <a href="javascript:void(0)">
+            <p className="font-bold italic text-2xl text-white">Arqum Ahmad</p>
+          </a>
+          <div className="md:hidden">
+            <button
+              className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
+              onClick={() => setState(!state)}
+            >
+              {state ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 8h16M4 16h16"
+                  />
+                </svg>
+              )}
+            </button>
           </div>
-      </nav>
-  )
+        </div>
+        <div
+          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+            state ? "block" : "hidden"
+          }`}
+        >
+          <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            {navigation.map((item, idx) => {
+              return (
+                <li
+                  key={idx}
+                  className="text-white hover:text-indigo-600 text-2xl"
+                >
+                  <a href={item.path}>{item.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="hidden md:inline-block">
+          <button class="contactButton">
+            Contact
+            <div class="iconButton">
+              <svg
+                height="24"
+                width="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path
+                  d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
