6f854b583a034347b4c6fd7e12ac4d6a21fd51ce
Arqum01 <arqumahmad019@gmail.com>
2024-05-31 23:54:18 +0500

    Update Heo saction gap

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <>
       <div className="bg-gray-900">
-        <section className="mt-48 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
+        <section className="mt-48 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex lg:gap-20 md:px-8">
           <div className="space-y-4 flex-1 sm:text-center lg:text-left">
             <h1 className="text-white font-bold text-4xl xl:text-5xl">
               Arqum Ahmad
@@ -36,7 +36,7 @@ function HeroSection() {
           <div className="flex-1 text-center mt-7 lg:mt-0 lg:ml-3">
             <img
               src="https://i.postimg.cc/HxHyt53c/undraw-heatmap-uyye.png"
-              className="w-full mx-auto sm:w-10/12  lg:w-full"
+              className="w-96 mx-auto sm:w-10/12  lg:w-full"
             />
           </div>
         </section>
