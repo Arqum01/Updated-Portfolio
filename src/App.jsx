1a6f9ebea3c5d2b6983144e2b721a82ed41c5b75
Arqum01 <arqumahmad019@gmail.com>
2024-06-09 23:47:46 +0500

    Footer but need to update it.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import { Navbar } from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar/>
       <HeroSection/>
+      <Footer/>
     </>
   )
 }
