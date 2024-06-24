51d50686a0c3c007665ae1767ec9841dae546ec8
Arqum01 <arqumahmad019@gmail.com>
2024-06-24 20:47:03 +0500

    Clients Experience section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Navbar } from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import Footer from './components/Footer'
 import CompanyExperience from './components/Experience/CompanyExperience'
+import ClientsExperience from './components/Experience/ClientsExperience'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <HeroSection/>
       <CompanyExperience/>
+      <ClientsExperience/>
       <Footer/>
     </>
   )
